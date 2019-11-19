3053210e3e48e950bb8ca1ec245490fa18731acc
lin zhu <chriszhu.cremona@outlook.com>
2019-11-19 10:54:44 -0500

    add testings for exceptions

--- tests/elementary_test.py
+++ tests/elementary_test.py
@@ -24,3 +24,26 @@ def test_trig2():
     x = ad.AutoDiff(2)
     func = ad.sin(x) + 2*ad.cosh(x) + 3 * ad.atan(x**2)
     assert(func.val, func.der, func.Jacobian()) == (np.sin(2) + 2 * np.cosh(2) + 3 * np.arctan(4), np.cos(2) + 2 * np.sinh(2) + 12/17, np.cos(2) + 2 * np.sinh(2) + 12/17)
+
+
+def test_npexp():
+    x = ad.AutoDiff(3)
+    func = ad.exp(4)*x
+    assert(func.val, func.der, func.Jacobian()) == (np.exp(4)*3, np.exp(4),np.exp(4))
+
+
+def test_nplog():
+    x = ad.AutoDiff(3)
+    func = ad.log(4)*x
+    assert(func.val, func.der, func.Jacobian()) == (np.log(4)*3, np.log(4),np.log(4))
+
+def test_npsqrt():
+    x = ad.AutoDiff(3)
+    func = ad.sqrt(4)*x
+    assert(func.val, func.der, func.Jacobian()) == (np.sqrt(4)*3, np.sqrt(4),np.sqrt(4))
+
+
+def test_npcosh():
+    x = ad.AutoDiff(3)
+    func = ad.cosh(4)*x
+    assert(func.val, func.der, func.Jacobian()) == (np.cosh(4)*3, np.cosh(4),np.cosh(4))
