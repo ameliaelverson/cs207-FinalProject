a20f49136824518335e3c2c31a77d3efb1018ff2
lin zhu <chriszhu.cremona@outlook.com>
2019-12-02 09:36:58 -0500

    First working Reverse Mode Basic Implementation

--- Extension/ReverseMode.py
+++ Extension/ReverseMode.py
@@ -1,5 +1,5 @@
 # Extension 
-
+from __future__ import division 
 import numpy as np
 
 
@@ -11,14 +11,23 @@ class ComputationalGraph:
 		self.NodeList =[]
 
 	def append(self, Node):
+		'''This method append the node to the graph in sequence and record the index of that node'''
 		self.Nodeindex +=1
 		self.NodeList.append(Node)
 
 	def ValidOp(self,i):  #Valid operation
+		'''
+		This method structurally defines a way to 
+		store all the valid operators 
+		supported by this reverse mode class 
+
+		'''
 		switcher ={
-			1 :"add",
+			1 : "add",
 			2 : "mul",
-			3 : "exp"
+			3 : "exp",
+			4 : "truediv",
+			5 : "pow"
 		}
 		return  switcher.get(i," Invalid Operator")
 
@@ -28,30 +37,39 @@ class ComputationalGraph:
 
 
 		'''
-		for nodes in NodeList:
-			if node.operation == ValidOp(1):
+		for node in self.NodeList:
+			if node.operation == self.ValidOp(1):
 				node.value = self.NodeList[node.nodeleft].value + self.NodeList[node.noderight].value
 
-			elif node.operation == ValidOp(2):
+			elif node.operation == self.ValidOp(2):
 				node.value = self.NodeList[node.nodeleft].value*self.NodeList[node.noderight].value
 
 
 	def ComputeGradient(self,lastIndex):
 		'''
-		This function back propagate to calculate the gradient of the variables
+		This function back propagate to calculate the gradient of the variables with reverse mode
 
 		'''
 		#Set the seed 
+		self.clearGraph()
 		self.NodeList[lastIndex].deri = 1.0
 		#Perform back prop
 		for node in self.NodeList[-1::-1]:
-			if node.operation == ValidOp(1):
+			if node.operation == self.ValidOp(1):
 				self.NodeList[node.nodeleft].deri += node.deri
-				self.NodeList[node.nodeleft].deri += node.deri
+				self.NodeList[node.noderight].deri += node.deri
 
-			elif node.operation == ValidOp(2):
+			elif node.operation == self.ValidOp(2):
 				self.NodeList[node.nodeleft].deri += node.deri * self.NodeList[node.noderight].value
 				self.NodeList[node.noderight].deri += node.deri * self.NodeList[node.nodeleft].value
+
+	def clearGraph(self):
+		'''
+		In order to reuse the node for different functions, 
+		we clear the graph by reassign the derivatives for all node to zero
+		'''
+		for i in self.NodeList:
+			i.deri = 0 
 
 
 
@@ -61,14 +79,18 @@ class Node:
 
 	
 
-	def __init__(self,value = None, deri = 0.0, index1 = None, index2 = None):
+	def __init__(self,value = None, Graph= None, deri = 0.0, index1 = None, index2 = None):
+
+		if Graph== None:
+			raise NotImplementError("Your Node is not Connected to any graph")
+
 		self.value = value
 		self.deri = deri
 		#if the node is left multiplied by another node, then the nodeleft is just node itself
 		self.nodeleft = index1
 		self.noderight = index2
 		self.Graph = Graph
-		self.ownindex = Graph.Nodeindex if self.Graph != None
+		if self.Graph != None: self.ownindex = Graph.Nodeindex 
 		#EveryTime we are creating a node, we put it into our graphs
 		self.Graph.append(self)
 
@@ -83,10 +105,13 @@ class Node:
 		Check if the node is a contant
 		Input:
 		x: A node or a constant 
+		if constant
 
 		'''
 		if not isinstance(x, cls):
-			return cls(value = x)
+			ConstantNode = cls(value = x,Graph = Graph)
+			ConstantNode.operation = "None"
+			return ConstantNode
 		else:
 			return x
 
@@ -95,8 +120,21 @@ class Node:
 		other = self.CheckConstant(other)
 		NewNode = Node(
 					value = self.value + other.value,
+					Graph = self.Graph,
 					index1 = self.ownindex,
-					index2 = other.ownindex
+					index2 = other.ownindex,
+					)
+		NewNode.operation = "add"
+		return NewNode
+
+
+	def __radd__(self,other):
+		other = self.CheckConstant(other)
+		NewNode = Node(
+					value = self.value + other.value,
+					Graph = self.Graph,
+					index1 = other.ownindex,
+					index2 = self.ownindex,
 					)
 		NewNode.operation = "add"
 		return NewNode
@@ -106,6 +144,7 @@ class Node:
 		other = self.CheckConstant(other)
 		NewNode = Node(
 					value = self.value*other.value,
+					Graph = self.Graph,
 					#record the index of the 2 nodes which produce this combined node
 					index1 = self.ownindex,
 					index2 = other.ownindex
@@ -114,9 +153,29 @@ class Node:
 		return NewNode
 
 
+	def __rmul__(self,other):
+		other = self.CheckConstant(other)
+		NewNode = Node(
+					value = self.value*other.value,
+					Graph = self.Graph,
+					#record the index of the 2 nodes which produce this combined node
+					index1 = other.ownindex,
+					index2 = self.ownindex
+					)
+		NewNode.operation = "mul"
+		return NewNode
 
 
 
 
+Graph = ComputationalGraph()
+X = Node(value = 3, Graph = Graph)
+Y = Node(value = 5, Graph = Graph)
 
+Graph.ComputeValue()
+
+
+
+from IPython import *
+embed()
 
