ab41b804ae1af839ee4bbbd67ee5f3d870d6bde9
EricaMoreira <ericamoreira@hsph.harvard.edu>
2019-12-05 01:33:00 -0500

    testing how to fix  __eq__ code

--- autodiff32/AutoDiffObj.py
+++ autodiff32/AutoDiffObj.py
@@ -88,10 +88,10 @@ class AutoDiff():
         	return "f(x) = {:.2f}, f'(x) = {:.2f}".format(self.val, self.der)
 
     def __eq__(self, other): # overload 'equal to' comparison operator
-        return (self.val == other.val and self.der == other.der)
+        return self.val == other.val and self.der == other.der
             
-    #def __ne__(self, other): # overload 'not equal to' comparison operator
-    #    return not self==other
+    def __ne__(self, other): # overload 'not equal to' comparison operator
+        return self.val != other.val or self.der != other.der
 
     def Jacobian(self):
         """
