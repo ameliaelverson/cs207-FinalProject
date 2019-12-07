f9441499bb0c589c4685f3a65b74f6de58e00938
EricaMoreira <ericamoreira@hsph.harvard.edu>
2019-12-07 17:03:18 -0500

    new version of multivariate var creator

--- autodiff32/MultivariateVarCreator.py
+++ autodiff32/MultivariateVarCreator.py
@@ -2,8 +2,9 @@ import numpy as np
 import autodiff32 as ad
 
 class Multi_AutoDiff_Creator:
+    
+    def __init__(self,Vals): #Now the user need to input values as a list with the first one specifying the first val for first val and so on
 
-    def __init__(self,*args,**kwargs):
         """Instantiates multiple AutoDiff objects
         
         INPUTS
@@ -34,12 +35,36 @@ class Multi_AutoDiff_Creator:
         >>> Y.der
         array([0., 1.])
         
-        """    
-        deri = np.identity(len(kwargs))
-        i = 0 
-        self.Vars =[]
-        for key, value in kwargs.items():
-            self.Vars.append(ad.AutoDiff(value,deri[i]))
-            i+=1
-            
+        #UserDEMO
+        X = [1,2,3]
+        Y = [2,3,3]
+        Z = [3,5,3]
+        W = [3,5,3]
+        VarValues = [X, Y, Z, W]
+        func = lambda Vars:3*Vars[0] + 4*Vars[1] + 4*Vars[2]**2 + 3*Vars[3]# Var[0] is X, Var[1] is Y
+        Values ,Derivatives = AutoDiff_Evaluate(VarValues,func)
         
+        """    
+#        deri = np.identity(len(kwargs))
+#        i = 0 
+#        self.Vars =[]
+#        for key, value in kwargs.items():
+#            self.Vars.append(ad.AutoDiff(value,deri[i]))
+#            i+=1
+
+        deri = np.identity(len(Vals))
+        self.Vars =[]
+        for i in range(len(Vals)):
+          self.Vars.append(ad.AutoDiff(Vals[i],deri[i]))          
+        
+
+
+def AutoDiff_Evaluate(Vals,f):
+    val = []
+    der = []
+    for i in np.transpose(Vals):
+        A = f(Multi_AutoDiff_Creator(i).Vars)
+        val.append(A.val)
+        der.append(A.der)
+  
+    return val, der
