7392b7abc7decb6ae82b234b1c705204964806be
EricaMoreira <ericamoreira@hsph.harvard.edu>
2019-12-07 13:58:58 -0500

    fixed spacing

--- autodiff32/Elementary.py
+++ autodiff32/Elementary.py
@@ -9,61 +9,61 @@ def exp(x):
     try:
         return AutoDiffObj.AutoDiff(np.exp(x.val), np.dot(np.exp(x.val),x.der))
     except Exception:
-        	return np.exp(x)
+        return np.exp(x)
 
 def logistic(x):
     try:
         return AutoDiffObj.AutoDiff(np.exp(x.val)/(np.exp(x.val) + 1), np.dot(np.exp(x.val)/((np.exp(x.val) + 1)**2),x.der))
     except Exception:
-        	return (np.exp(x)/(np.exp(x) + 1))
+        return (np.exp(x)/(np.exp(x) + 1))
 
 def log(x, base=np.e): # accepts any value for the base, but default is natural log
     try:
         return AutoDiffObj.AutoDiff(np.log(x.val)/np.log(base), (1/(np.log(base) * x.val)) * x.der)
     except Exception:
-        	return np.log(x)/np.log(base)
+        return np.log(x)/np.log(base)
     
 def sqrt(x):
     try:
         return AutoDiffObj.AutoDiff(np.sqrt(x.val), x.der * 1/(2*np.sqrt(x.val)))
     except Exception:
-        	return x**0.5
+        return x**0.5
     
 def sin(x):
     try:
         return AutoDiffObj.AutoDiff(np.sin(x.val), np.cos(x.val) * x.der)
     except Exception:
-        	return np.sin(x)
+        return np.sin(x)
     
 def cos(x):
     try:
         return AutoDiffObj.AutoDiff(cos(x.val), -sin(x.val) * x.der)
     except Exception:
-        	return np.cos(x)
+        return np.cos(x)
        
 def tan(x):
     try:
         return AutoDiffObj.AutoDiff(tan(x.val), 1/(cos(x.val) ** 2) * x.der)
     except Exception:
-        	return np.tan(x)
+        return np.tan(x)
     
 def asin(x):
     try:
         return AutoDiffObj.AutoDiff(asin(x.val), (1/np.sqrt(1 - x.val**2)) * x.der)
     except Exception:
-        	return np.arcsin(x)
+        return np.arcsin(x)
     
 def acos(x):
     try:
         return AutoDiffObj.AutoDiff(acos(x.val), (-1/np.sqrt(1 - x.val ** 2)) * x.der)
     except Exception:
-        	return np.arccos(x)
+        return np.arccos(x)
     
 def atan(x):
     try:
         return AutoDiffObj.AutoDiff(atan(x.val), (1/(1 + x.val ** 2)) * x.der)
     except Exception:
-        	return np.arctan(x)
+        return np.arctan(x)
 
 def sinh(x):
     try:
