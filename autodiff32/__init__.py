f75cf1072e381c5799925aa3596e10e7e3c173ad
aaronjj908 <aaronjacobson@g.harvard.edu>
2019-12-09 04:32:26 -0500

    updated init

--- autodiff32/__init__.py
+++ autodiff32/__init__.py
@@ -3,4 +3,8 @@ from .AutoDiffObj import AutoDiff
 from .Elementary import *
 from .JacobianVectorFunc import Jacobian
 from .MultivariateVarCreator import Multi_AutoDiff_Creator
+from .Graph import ComputationalGraph
+from .ElementaryReverse import *
+from .ReverseJacobVectorFunc import ReverseVecFunc
+from .ReverseMode import Node
 
