6e4f265c220a2a156bab51e7717878dea252256c
Erica Moreira <ericamoreira@hsph.harvard.edu>
2019-12-10 10:00:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name = "autodiff32",
-	version = "0.1.0",
+	version = "0.1.2",
 	author = "Amelia Elverson, Aaron Jacobson, Eric Moreira, Lin Zhu",
 	author_email = "aelverson@mba2020.hbs.edu",
 	description = "Automatic Differentiator",
